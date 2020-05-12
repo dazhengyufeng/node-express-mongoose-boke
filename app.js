6327ab9a9b0c608a2669c344e129192f35210eac
郑雨峰 <3057542870@qq.com>
2020-05-12 17:24:23 +0800

    ..

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const session = require('express-session');
-
+const cors = require('cors');
 // swagger1
 // const swaggerUi = require('swagger-ui-express')
 // const swaggerJSDoc = require('swagger-jsdoc')
@@ -17,14 +17,14 @@ const app = express();
 const expressSwagger = require('express-swagger-generator')(app);
 
 //设置跨域访问
-app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By",' 3.2.1')
-    res.header("Content-Type", "application/json;charset=utf-8");
-    next();
-});
+// app.all('*', function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+//     res.header("X-Powered-By",' 3.2.1')
+//     res.header("Content-Type", "application/json;charset=utf-8");
+//     next();
+// });
 
 // swagger2
 // 配置 swagger-jsdoc
@@ -74,7 +74,7 @@ expressSwagger(options)
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(cors);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
